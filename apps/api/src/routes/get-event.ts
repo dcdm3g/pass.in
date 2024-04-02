04e4821ab9f859d1ef5f5ec04e10a3fbeb6eec9d
Davi Melo <davicosta01082008@gmail.com>
2024-04-02 15:24:17 -0300

    feat(api): add get event route

---
+++ apps/api/src/routes/get-event.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+export async function getEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/events/:id',
+    {
+      schema: {
+        params: z.object({ id: z.string().uuid() }),
+        response: {
+          200: z.object({
+            event: z.object({
+              title: z.string().min(4),
+              details: z.string().nullish(),
+              slug: z.string(),
+              maximumAttendees: z.number().int().positive().nullish(),
+              amountOfAttendees: z.number().int().nonnegative(),
+            }),
+          }),
+          404: z.object({ message: z.string() }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params
+
+      const event = await prisma.event.findUnique({
+        where: { id },
+        select: {
+          title: true,
+          details: true,
+          slug: true,
+          maximumAttendees: true,
+          _count: { select: { attendees: true } },
+        },
+      })
+
+      if (!event) {
+        return reply.status(404).send({
+          message: 'Event not found',
+        })
+      }
+
+      return {
+        event: {
+          title: event.title,
+          details: event.details,
+          slug: event.slug,
+          maximumAttendees: event.maximumAttendees,
+          amountOfAttendees: event._count.attendees,
+        },
+      }
+    },
+  )
+}
