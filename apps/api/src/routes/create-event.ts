eed4bc5b1631738c5db18bd90a87c0ba33bb51f4
Davi Melo <davicosta01082008@gmail.com>
2024-04-02 13:10:46 -0300

    feat(api): add create event route

---
+++ apps/api/src/routes/create-event.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+import { slugify } from '@/utils/slugify'
+
+export async function createEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/events',
+    {
+      schema: {
+        body: z.object({
+          title: z.string().min(4),
+          details: z.string().nullish(),
+          maximumAttendees: z.number().int().positive().nullish(),
+        }),
+        response: {
+          201: z.object({
+            eventId: z.string().uuid(),
+          }),
+          409: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { title, details, maximumAttendees } = request.body
+
+      const slug = slugify(title)
+
+      const eventWithSameSlug = await prisma.event.findUnique({
+        where: { slug },
+      })
+
+      if (eventWithSameSlug) {
+        return reply.status(409).send({
+          message: 'Slug already in use',
+        })
+      }
+
+      const event = await prisma.event.create({
+        data: {
+          title,
+          details,
+          maximumAttendees,
+          slug,
+        },
+        select: {
+          id: true,
+        },
+      })
+
+      return reply.status(201).send({ eventId: event.id })
+    },
+  )
+}
